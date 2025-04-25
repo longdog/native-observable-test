af8a9ce6d63ac5159074f653eac3324dd4633b44
longdog <linda@longdog.us>
2025-04-25 08:41:41 +0400

    sync and async subsribers

--- src/main.js
+++ src/main.js
@@ -1,24 +1,34 @@
-import './style.css'
-
+import "./style.css";
 
 function setupCounter(element) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.when('click').subscribe(() => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.when("click").subscribe(() => setCounter(counter + 1));
+  setCounter(0);
 }
 
+function subsr() {
+  const nums = new Observable((subscriber) => {
+    subscriber.next(1);
+    setTimeout(() => subscriber.next(2));
+  });
 
-document.querySelector('#app').innerHTML = `
+  nums.subscribe((n) => console.log("new data for subscriber 1:", n));
+
+  nums.subscribe((n) => console.log("new data for subscriber 2:", n));
+}
+
+document.querySelector("#app").innerHTML = `
   <div>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
   </div>
-`
+`;
 
+setupCounter(document.querySelector("#counter"));
 
-setupCounter(document.querySelector('#counter'))
+subsr();
